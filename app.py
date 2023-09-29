f9b4667dbfce14f4ef273e9bde142aa00002f4f1
Marcos Simon <67840553+AlexMack1@users.noreply.github.com>
2023-09-29 00:50:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,43 @@
 import streamlit as st
-# Asumiendo que tus algoritmos están en archivos llamados codigo1.py, codigo2.py, etc.
-import codigo1
-import codigo2
-import codigo3
-import codigo4
+
+def modelo1():
+    # Aquí va todo el código adaptado a Streamlit del primer modelo/algoritmo.
+    # ...
+    st.write("Resultados del Modelo 1")
+
+def modelo2():
+    # Aquí va todo el código adaptado a Streamlit del segundo modelo/algoritmo.
+    # ...
+    st.write("Resultados del Modelo 2")
+
+def modelo3():
+    # Aquí va todo el código adaptado a Streamlit del tercer modelo/algoritmo.
+    # ...
+    st.write("Resultados del Modelo 3")
+
+def modelo4():
+    # Aquí va todo el código adaptado a Streamlit del cuarto modelo/algoritmo.
+    # ...
+    st.write("Resultados del Modelo 4")
 
 def main():
-    st.title("Selección de Algoritmo para Cosecha de Caña")
+    st.title("Selección de Modelo/Algoritmo")
 
-    algoritmo = st.selectbox("Elija un algoritmo:", ["Algoritmo 1", "Algoritmo 2", "Algoritmo 3", "Algoritmo 4"])
-    
-    if algoritmo == "Algoritmo 1":
-        # Solicitar parámetros específicos del algoritmo 1
-        # Por ejemplo:
-        N = st.number_input("Ingrese el número de parcelas (N):", min_value=1)
-        # (repite para otros parámetros si es necesario)
-        if st.button("Ejecutar"):
-            result = codigo1.ejecutar(N)  # Asumiendo que 'ejecutar' es una función que haz definido en codigo1.py
-            st.write(result)
-        
-    elif algoritmo == "Algoritmo 2":
-        # Repite lo anterior para cada algoritmo
-        pass  # (solo un marcador de posición)
+    # Crear un desplegable con los modelos/algoritmos disponibles.
+    opcion = st.selectbox(
+        'Seleccione el modelo o algoritmo que desea ejecutar:',
+        ('Modelo 1', 'Modelo 2', 'Modelo 3', 'Modelo 4')
+    )
 
-    # (repite para los algoritmos 3 y 4)
+    if st.button("Ejecutar"):
+        if opcion == 'Modelo 1':
+            modelo1()
+        elif opcion == 'Modelo 2':
+            modelo2()
+        elif opcion == 'Modelo 3':
+            modelo3()
+        elif opcion == 'Modelo 4':
+            modelo4()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
