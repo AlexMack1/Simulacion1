2cf1cafdd96e8d2854ca2f4378a70b539c72404c
Marcos Simon <67840553+AlexMack1@users.noreply.github.com>
2023-09-29 00:20:18 -0600

    Update codigo1.py

--- codigo1.py
+++ codigo1.py
@@ -1,23 +1,28 @@
+import streamlit as st
 import random
 import matplotlib.pyplot as plt
 
-# Solicitar parámetros iniciales
-N = int(input("Ingrese el número de parcelas (N): "))
+# Configuración inicial
+st.title("Optimización de Cosecha con Algoritmo Genético")
+st.write("Ajusta los parámetros y ejecuta el algoritmo genético para maximizar la cosecha.")
+
+# Entradas
+N = st.sidebar.slider("Número de parcelas (N)", 1, 100, 10)
 
 V = [random.randint(100, 1200) for _ in range(N)]
 P = [random.uniform(1.0, 10.0) for _ in range(N)]
 
-num_vehiculos = int(input("Ingrese el número de vehículos de transporte: "))
-C = [int(input(f"Ingrese la capacidad máxima de carga del vehículo {i+1} (entre 1000 y 2500): ")) for i in range(num_vehiculos)]
+num_vehiculos = st.sidebar.slider("Número de vehículos de transporte", 1, 10, 1)
+C = [st.sidebar.slider(f"Capacidad máxima de carga del vehículo {i+1}", 1000, 2500, 1500) for i in range(num_vehiculos)]
 
-num_trabajadores = int(input("Ingrese el número total de trabajadores: "))
+num_trabajadores = st.sidebar.slider("Número total de trabajadores", 1, 100, 10)
 W = 40 * num_trabajadores
 
 # Parámetros del algoritmo genético
 POBLACION_SIZE = 100
 GENES = "01"
-GENERACIONES = 1000
-PROBABILIDAD_MUTACION = 0.2
+GENERACIONES = st.sidebar.slider("Número de generaciones", 100, 5000, 1000)
+PROBABILIDAD_MUTACION = st.sidebar.slider("Probabilidad de mutación", 0.0, 1.0, 0.2)
 
 def individuo_inicial(n):
     return ''.join(random.choice(GENES) for _ in range(n))
@@ -34,7 +39,6 @@ def fitness(individuo):
     return total_valor
 
 def seleccionar_padres(poblacion):
-    # Seleccionamos dos individuos basados en su aptitud (ruleta)
     weights = [fitness(ind) for ind in poblacion]
     return random.choices(poblacion, weights=weights, k=2)
 
@@ -45,65 +49,51 @@ def crossover(padres):
     return hijo1, hijo2
 
 def mutacion(individuo):
-    # Invertimos algunos bits con una probabilidad dada
     return ''.join(g if random.random() > PROBABILIDAD_MUTACION else random.choice(GENES) for g in individuo)
 
 # Algoritmo Genético
-poblacion = [individuo_inicial(N) for _ in range(POBLACION_SIZE)]
+if st.button("Ejecutar Algoritmo Genético"):
+    poblacion = [individuo_inicial(N) for _ in range(POBLACION_SIZE)]
+    mejores_aptitudes = []
 
-# Lista para almacenar la mejor aptitud de cada generación
-mejores_aptitudes = []
+    for generacion in range(GENERACIONES):
+        nueva_poblacion = []
 
-for generacion in range(GENERACIONES):
-    nueva_poblacion = []
+        while len(nueva_poblacion) < POBLACION_SIZE:
+            padres = seleccionar_padres(poblacion)
+            hijos = crossover(padres)
+            nueva_poblacion.append(mutacion(hijos[0]))
+            nueva_poblacion.append(mutacion(hijos[1]))
+
+        poblacion = nueva_poblacion
+        mejor_aptitud_generacion = max([fitness(ind) for ind in poblacion])
+        mejores_aptitudes.append(mejor_aptitud_generacion)
+
+    mejor_individuo = max(poblacion, key=fitness)
+    valor_mejor_individuo = fitness(mejor_individuo)
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.plot(mejores_aptitudes)
+    ax.set_title("Evolución de la aptitud máxima por generación")
+    ax.set_xlabel("Generaciones")
+    ax.set_ylabel("Mejor aptitud")
+    ax.grid(True)
+    texto = f"Mejor solución: {mejor_individuo}\nAptitud: {valor_mejor_individuo}"
+    ax.annotate(texto, xy=(GENERACIONES, valor_mejor_individuo), xycoords='data',
+                 xytext=(-150, 20), textcoords='offset points',
+                 arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"),
+                 bbox=dict(boxstyle="round,pad=0.3", edgecolor="black", facecolor="aliceblue"))
+    st.pyplot(fig)
+
+    st.subheader("Explicación:")
+    st.write("Cada individuo en nuestra población está representado por una cadena de '0's y '1's.")
+    st.write("La longitud de esta cadena es igual al número de parcelas N.")
+    st.write("- Un '1' en la posición i significa que la parcela i ha sido seleccionada para la cosecha.")
+    st.write("- Un '0' en la posición i significa que la parcela i no ha sido seleccionada para la cosecha.")
     
-    while len(nueva_poblacion) < POBLACION_SIZE:
-        padres = seleccionar_padres(poblacion)
-        hijos = crossover(padres)
-        nueva_poblacion.append(mutacion(hijos[0]))
-        nueva_poblacion.append(mutacion(hijos[1]))
-    
-    poblacion = nueva_poblacion
-    
-    # Almacenamos la mejor aptitud de esta generación
-    mejor_aptitud_generacion = max([fitness(ind) for ind in poblacion])
-    mejores_aptitudes.append(mejor_aptitud_generacion)
+    st.subheader("Interpretación de la solución:")
+    st.write(f"La aptitud máxima obtenida es {valor_mejor_individuo}.")
+    st.write("Esto significa que, dada la selección óptima de parcelas, la cantidad total de caña de azúcar que puede ser cosechada y transportada sin exceder las restricciones es de", valor_mejor_individuo, "kilogramos.")
+    parcelas_seleccionadas = [i+1 for i, bit in enumerate(mejor_individuo) if bit == '1']
+    st.write("Las parcelas seleccionadas para lograr esta cosecha son:", parcelas_seleccionadas)
 
-
-
-
-# Al final de las generaciones, obtenemos la mejor solución:
-mejor_individuo = max(poblacion, key=fitness)
-valor_mejor_individuo = fitness(mejor_individuo)
-
-# Gráfica de la evolución de la aptitud
-plt.figure(figsize=(12, 6))
-plt.plot(mejores_aptitudes)
-plt.title("Evolución de la aptitud máxima por generación")
-plt.xlabel("Generaciones")
-plt.ylabel("Mejor aptitud")
-plt.grid(True)
-
-# Indicamos la solución en la gráfica
-texto = f"Mejor solución: {mejor_individuo}\nAptitud: {valor_mejor_individuo}"
-plt.annotate(texto, xy=(GENERACIONES, valor_mejor_individuo), xycoords='data',
-             xytext=(-150, 20), textcoords='offset points',
-             arrowprops=dict(arrowstyle="->",
-                             connectionstyle="arc3,rad=.2"),
-             bbox=dict(boxstyle="round,pad=0.3", edgecolor="black", facecolor="aliceblue"))
-
-plt.show()
-
-# Explicación sobre la representación de los individuos:
-print("\nEXPLICACIÓN:")
-print("Cada individuo en nuestra población está representado por una cadena de '0's y '1's.")
-print("La longitud de esta cadena es igual al número de parcelas N.")
-print("- Un '1' en la posición i significa que la parcela i ha sido seleccionada para la cosecha.")
-print("- Un '0' en la posición i significa que la parcela i no ha sido seleccionada para la cosecha.")
-
-# Explicación sobre la aptitud y la interpretación de la solución:
-print("\nINTERPRETACIÓN DE LA SOLUCIÓN:")
-print(f"La aptitud máxima obtenida es {valor_mejor_individuo}.")
-print("Esto significa que, dada la selección óptima de parcelas, la cantidad total de caña de azúcar que puede ser cosechada y transportada sin exceder las restricciones es de", valor_mejor_individuo, "kilogramos.")
-parcelas_seleccionadas = [i+1 for i, bit in enumerate(mejor_individuo) if bit == '1']
-print("Las parcelas seleccionadas para lograr esta cosecha son:", parcelas_seleccionadas)
