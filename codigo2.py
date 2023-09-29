2d5adfcbab7077ba462d65490138ff2b80efeef3
Marcos Simon <67840553+AlexMack1@users.noreply.github.com>
2023-09-29 00:38:35 -0600

    Update codigo2.py

--- codigo2.py
+++ codigo2.py
@@ -1,25 +1,19 @@
 import numpy as np
 import random
 import math
-import matplotlib.pyplot as plt
+import streamlit as st
 
-# Inicialización del problema basado en la entrada del usuario
-def init_problem_from_user():
-    N = int(input("Ingrese el número de parcelas (N): "))
+# Función para inicializar el problema desde widgets de Streamlit
+def init_problem():
+    N = st.sidebar.slider("Número de parcelas (N)", 1, 100, 50)
+    num_vehiculos = st.sidebar.slider("Número de vehículos de transporte", 1, 10, 5)
+    capacities = st.sidebar.slider("Capacidad máxima de carga de los vehículos", 1000, 2500, 1500)
+    num_trabajadores = st.sidebar.slider("Número total de trabajadores", 1, 50, 25)
 
     # Generando las parcelas
     Vi = np.random.randint(100, 1201, size=N)  # Valor de caña de azúcar por parcela
     Pi = np.random.uniform(1.0, 10.1, size=N)  # Tiempo necesario para cosechar
-    
-    num_vehiculos = int(input("Ingrese el número de vehículos de transporte: "))
-    capacities = []
-    for i in range(num_vehiculos):
-        capacity = int(input(f"Ingrese la capacidad máxima de carga del vehículo {i+1} (entre 1000 y 2500): "))
-        capacities.append(capacity)
-
-    C = sum(capacities)  # Suma total de capacidades de todos los vehículos
-
-    num_trabajadores = int(input("Ingrese el número total de trabajadores: "))
+    C = capacities * num_vehiculos
     W = num_trabajadores * 40  # Total de horas de trabajo disponibles
     
     return Vi, Pi, C, W
@@ -80,45 +74,34 @@ def interpret_results(best_solution, best_value, Vi, Pi, C, W):
     total_peso = np.sum(Pi * best_solution)
     num_parcelas_cosechadas = np.sum(best_solution)
     
-    print("\nInterpretación de los resultados:")
-    print("---------------------------------")
-    print(f"Se ha decidido cosechar {num_parcelas_cosechadas} de las {len(Vi)} parcelas disponibles.")
-    print(f"El valor total obtenido de la caña recolectada es: {best_value}.")
-    print(f"Esto se traduce en un tiempo total de cosecha de {total_peso} horas.")
+    st.subheader("Interpretación de los resultados")
+    st.write(f"Se ha decidido cosechar {num_parcelas_cosechadas} de las {len(Vi)} parcelas disponibles.")
+    st.write(f"El valor total obtenido de la caña recolectada es: {best_value}.")
+    st.write(f"Esto se traduce en un tiempo total de cosecha de {total_peso} horas.")
     if total_peso <= W:
-        print(f"El tiempo total de cosecha está dentro del límite de trabajo disponible de {W} horas.")
+        st.write(f"El tiempo total de cosecha está dentro del límite de trabajo disponible de {W} horas.")
     else:
-        print(f"¡Advertencia! El tiempo total de cosecha excede el límite de trabajo disponible de {W} horas.")
+        st.warning(f"¡Advertencia! El tiempo total de cosecha excede el límite de trabajo disponible de {W} horas.")
     if best_value <= C:
-        print(f"El valor total de la caña recolectada está dentro de la capacidad de carga del vehículo de {C}.")
+        st.write(f"El valor total de la caña recolectada está dentro de la capacidad de carga del vehículo de {C}.")
     else:
-        print(f"¡Advertencia! El valor total de la caña recolectada excede la capacidad de carga del vehículo de {C}.")
-    
-    print("\nInterpretación de la gráfica:")
-    print("------------------------------")
-    print("La gráfica muestra cómo evoluciona el mejor valor de caña recolectada a lo largo de las iteraciones del algoritmo.")
-    print("Esto da una idea de cómo el algoritmo explora el espacio de soluciones y cuán rápidamente encuentra una solución óptima o cerca de óptima.")
-    print("Si la gráfica muestra una tendencia ascendente, significa que con el tiempo, el algoritmo está encontrando soluciones con valores más altos.")
-    print("Si la gráfica se estabiliza, indica que el algoritmo ha encontrado una solución que considera óptima en el contexto de las restricciones.")
+        st.warning(f"¡Advertencia! El valor total de la caña recolectada excede la capacidad de carga del vehículo de {C}.")
 
-# Definición de las variables y ejecución del algoritmo
-Vi, Pi, C, W = init_problem_from_user()
+if __name__ == '__main__':
+    st.title("Optimización de Cosecha con Recocido Simulado")
 
-temp = 100
-cooling_factor = 0.95
-iterations = 1000
+    Vi, Pi, C, W = init_problem()
 
-best_solution, best_value, fitness_evolution = simulated_annealing(Vi, Pi, C, W, temp, cooling_factor, iterations)
+    temp = 100
+    cooling_factor = 0.95
+    iterations = 1000
 
-# Graficando la evolución del fitness
-plt.figure(figsize=(12, 6))
-plt.plot(fitness_evolution)
-plt.title("Evolución del Fitness en el Recocido Simulado")
-plt.xlabel("Iteraciones")
-plt.ylabel("Valor de la Función Objetivo (Fitness)")
-plt.grid(True)
-plt.show()
+    if st.button("Ejecutar Recocido Simulado"):
+        best_solution, best_value, fitness_evolution = simulated_annealing(Vi, Pi, C, W, temp, cooling_factor, iterations)
 
-print("Mejor solución:", best_solution)
-print("Mejor valor:", best_value)
-interpret_results(best_solution, best_value, Vi, Pi, C, W)
+        # Graficando la evolución del fitness en Streamlit
+        st.line_chart(fitness_evolution, use_container_width=True)
+
+        st.write("Mejor solución:", best_solution)
+        st.write("Mejor valor:", best_value)
+        interpret_results(best_solution, best_value, Vi, Pi, C, W)
